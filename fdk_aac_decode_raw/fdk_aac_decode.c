056dfd4031f2d76886e6cacaac0a0a0518713c5a
zhiyuan.liu <zhiyuan.liu@seraphic-corp.com>
2023-11-01 00:54:25 +0000

    collect test data and foramt code

--- fdk_aac_decode_raw/fdk_aac_decode.c
+++ fdk_aac_decode_raw/fdk_aac_decode.c
@@ -4,42 +4,53 @@
 #include <string.h>
 #include "aacdecoder_lib.h"
 
-#define DECODER_BUFFSIZE        (2048 * sizeof(INT_PCM))
-#define RAW_DATA_BUF_MAX_SIZE   1024
-#define MAX_CHANNELS            6
+#define DECODER_BUFFSIZE (2048 * sizeof(INT_PCM))
+#define RAW_DATA_BUF_MAX_SIZE 1024
+#define MAX_CHANNELS 6
 
-#define AAC_ELD_FILENAME_PATTERN    "../AAC-ELD/dump_%d.raw"
-#define AAC_LC_FILENAME_PATTERN     "../AAC-LC/dump_%d.raw"
-#define PCM_OUTPUT_FILE_NAME        "./output.pcm"
+#define AAC_ELD_FILENAME_PATTERN "../AAC-ELD/dump_%d.raw"
+#define AAC_LC_FILENAME_PATTERN "./AAC-LC/dump_%d.raw"
+#define AAC_LC_SBR_FILENAME_PATTERN "./AAC-LC-SBR/dump_%d.raw"
+#define PCM_OUTPUT_FILE_NAME "./output.pcm"
 
-int g_decode_aac_profile = AOT_ER_AAC_ELD;
-unsigned char* g_raw_buf_list[RAW_DATA_BUF_MAX_SIZE];
+int g_decode_aac_profile = AOT_SBR;
+unsigned char *g_raw_buf_list[RAW_DATA_BUF_MAX_SIZE];
 int g_raw_buf_size_list[RAW_DATA_BUF_MAX_SIZE];
 int raw_buf_total_cout = 0;
 
-
 void prepare_aac_raw_buf_list()
 {
-  memset(g_raw_buf_list, 0, sizeof(unsigned char*) * RAW_DATA_BUF_MAX_SIZE);
+  memset(g_raw_buf_list, 0, sizeof(unsigned char *) * RAW_DATA_BUF_MAX_SIZE);
   memset(g_raw_buf_size_list, 0, sizeof(int) * RAW_DATA_BUF_MAX_SIZE);
 
   char dfilename[64];
 
   int bread = 0;
 
-  while (1) {
+  while (1)
+  {
     memset(dfilename, 0, 64);
 
-    if (g_decode_aac_profile == AOT_ER_AAC_ELD) {
+    if (g_decode_aac_profile == AOT_ER_AAC_ELD)
+    {
       sprintf(dfilename, AAC_ELD_FILENAME_PATTERN, raw_buf_total_cout);
-    } else if (g_decode_aac_profile == AOT_AAC_LC) {
+    }
+    else if (g_decode_aac_profile == AOT_AAC_LC)
+    {
       sprintf(dfilename, AAC_LC_FILENAME_PATTERN, raw_buf_total_cout);
-    } else {
+    }
+    else if (g_decode_aac_profile == AOT_SBR)
+    {
+      sprintf(dfilename, AAC_LC_SBR_FILENAME_PATTERN, raw_buf_total_cout);
+    }    
+    else
+    {
       exit(1);
     }
 
-    FILE* fp = fopen(dfilename, "rb");
-    if (!fp) {
+    FILE *fp = fopen(dfilename, "rb");
+    if (!fp)
+    {
       printf("aac raw buf prepare ok\n");
       break;
     }
@@ -49,7 +60,7 @@ void prepare_aac_raw_buf_list()
     rewind(fp);
 
     assert(g_raw_buf_list[raw_buf_total_cout] == NULL);
-    g_raw_buf_list[raw_buf_total_cout] = (unsigned char*)malloc(sizeof(unsigned char) * rs);
+    g_raw_buf_list[raw_buf_total_cout] = (unsigned char *)malloc(sizeof(unsigned char) * rs);
     assert(g_raw_buf_list[raw_buf_total_cout]);
     memset(g_raw_buf_list[raw_buf_total_cout], 0, rs);
 
@@ -61,7 +72,7 @@ void prepare_aac_raw_buf_list()
     // printf("process buf idx: %d ok\n", raw_buf_total_cout);
     raw_buf_total_cout++;
 
-    if(raw_buf_total_cout >= RAW_DATA_BUF_MAX_SIZE-1)
+    if (raw_buf_total_cout >= RAW_DATA_BUF_MAX_SIZE - 1)
     {
       printf("already prepared max aac raw buffer we can support\n");
       break;
@@ -71,13 +82,14 @@ void prepare_aac_raw_buf_list()
 
 void release_aac_raw_buf_list()
 {
-  for (int i = 0; i < RAW_DATA_BUF_MAX_SIZE; i++) {
-    if (g_raw_buf_list[i]) {
+  for (int i = 0; i < RAW_DATA_BUF_MAX_SIZE; i++)
+  {
+    if (g_raw_buf_list[i])
+    {
       free(g_raw_buf_list[i]);
     }
   }
 }
-
 
 int main()
 {
@@ -86,30 +98,48 @@ int main()
   assert(decoder);
 
   // 设置ASC信息
-  if (g_decode_aac_profile == AOT_ER_AAC_ELD) {
-    UCHAR conf[] = {0xF8, 0xF0, 0x20, 0x00};  //AAL-ELD 16000kHz MONO
-    UCHAR* conf_array[1] = { conf };
+  if (g_decode_aac_profile == AOT_ER_AAC_ELD)
+  {
+    UCHAR conf[] = {0xF8, 0xF0, 0x20, 0x00}; //AAL-ELD 16000kHz MONO
+    UCHAR *conf_array[1] = {conf};
     UINT length = 4;
     err = aacDecoder_ConfigRaw(decoder, conf_array, &length);
     assert(!err);
-  } else if (g_decode_aac_profile == AOT_AAC_LC) {
-    UCHAR conf[] = {0x12, 0x10};  //AAL-LC 44100kHz STEREO
+  }
+  else if (g_decode_aac_profile == AOT_AAC_LC)
+  {
+
+#if 1
+    UCHAR conf[] = {0x11, 0x90, 0x00, 0x07};  //AAL-LC 44100kHz STEREO
     UCHAR* conf_array[1] = { conf };
+    UINT length = 4;
+#else
+    UCHAR conf[] = {0x12, 0x10}; //AAL-LC 44100kHz STEREO
+    UCHAR *conf_array[1] = {conf};
     UINT length = 2;
+#endif
+
     err = aacDecoder_ConfigRaw(decoder, conf_array, &length);
     assert(!err);
   }
+  else if (g_decode_aac_profile == AOT_SBR)
+  {
+    UCHAR conf[] = {0x49, 0x90}; //AAL-LC-SBR 48kHz 2 channels
+    UCHAR *conf_array[1] = {conf};
+    UINT length = 2;
+    err = aacDecoder_ConfigRaw(decoder, conf_array, &length);
+    assert(!err);    
+  }
 
   // 获取信息
-  CStreamInfo* info = aacDecoder_GetStreamInfo(decoder);
+  CStreamInfo *info = aacDecoder_GetStreamInfo(decoder);
   assert(info);
-  // printf("aacSampleRate: %d\n", info->aacSampleRate);
-  // printf("channelConfig: %d\n", info->channelConfig);
-  // printf("aot: %d\n", info->aot);
+  printf("aacSampleRate: %d\n", info->aacSampleRate);
+  printf("channelConfig: %d\n", info->channelConfig);
+  printf("aot: %d\n", info->aot);
 
   // 构建AAC Raw Data Buffer List
   prepare_aac_raw_buf_list();
-
 
   // pcm输出buffer的大小可以参考CStreamInfo中frameSize的定义
   // typedef struct {
@@ -123,27 +153,35 @@ int main()
   //...
   //}CStreamInfo
   int max_frame_size;
-  if (g_decode_aac_profile == AOT_ER_AAC_ELD) {
+  if (g_decode_aac_profile == AOT_ER_AAC_ELD)
+  {
     max_frame_size = 512;
-  } else if (g_decode_aac_profile == AOT_AAC_LC) {
+  }
+  else if (g_decode_aac_profile == AOT_AAC_LC)
+  {
     max_frame_size = 1024;
+  }
+  else if (g_decode_aac_profile == AOT_SBR)
+  {
+    max_frame_size = 2048;
   }
 
   int pcm_buffer_size = max_frame_size * MAX_CHANNELS;
-  INT_PCM* pcm_buffer = (INT_PCM*)malloc(sizeof(INT_PCM) * pcm_buffer_size);
+  INT_PCM *pcm_buffer = (INT_PCM *)malloc(sizeof(INT_PCM) * pcm_buffer_size);
   assert(pcm_buffer);
 
-  FILE* output_fp = fopen(PCM_OUTPUT_FILE_NAME, "wb");
+  FILE *output_fp = fopen(PCM_OUTPUT_FILE_NAME, "wb");
   assert(output_fp);
-
 
   // 开始解码循环
   int cur_raw_buf_index = 0;
   UINT flags = 0;
   UINT bytes_valid;
-  do {
+  do
+  {
 
-    if (cur_raw_buf_index >= raw_buf_total_cout) {
+    if (cur_raw_buf_index >= raw_buf_total_cout)
+    {
       printf("decode end\n");
       break;
     }
@@ -160,15 +198,16 @@ int main()
     assert(err == AAC_DEC_OK);
 
     // 通过获取信息，计算实际输出的pcm数据大小
-    CStreamInfo* info = aacDecoder_GetStreamInfo(decoder);
+    CStreamInfo *info = aacDecoder_GetStreamInfo(decoder);
     assert(info);
-    // printf("sampleRate: %d\n", info->sampleRate);
-    // printf("frameSize: %d\n", info->frameSize);
-    // printf("numChannels: %d\n", info->numChannels);
+    printf("sampleRate: %d\n", info->sampleRate);
+    printf("frameSize: %d\n", info->frameSize);
+    printf("numChannels: %d\n", info->numChannels);
     int output_pcm_bytes = info->frameSize * info->numChannels * 2;
 
     // pcm 数据写入文件
-    if (output_fp) {
+    if (output_fp)
+    {
       size_t ws = fwrite(pcm_buffer, output_pcm_bytes, 1, output_fp);
       assert(ws > 0);
     }
@@ -177,10 +216,12 @@ int main()
   } while (1);
 
   // 释放资源
-  if (output_fp) {
+  if (output_fp)
+  {
     fclose(output_fp);
   }
-  if (pcm_buffer) {
+  if (pcm_buffer)
+  {
     free(pcm_buffer);
   }
   release_aac_raw_buf_list();
